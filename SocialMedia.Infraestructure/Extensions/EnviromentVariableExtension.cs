2f874eb267f6475a4c605b5e56a0973bb6084eb9
Keny Travanino <keny03@unitec.edu>
2024-01-31 21:48:12 -0600

    add enviroment variables extension

---
+++ SocialMedia.Infraestructure/Extensions/EnviromentVariableExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Extensions
+{
+    public static class EnviromentVariableExtension
+    {
+        public static string GetFromEnviroment(this IConfiguration configuration, string name)
+        {
+            var variables = Environment.GetEnvironmentVariables();
+            string connectionString = configuration.GetConnectionString(name) ?? string.Empty;
+            StringBuilder stringBuilder = new StringBuilder(connectionString);
+            foreach (DictionaryEntry variable in variables)
+            {
+                if(connectionString.Contains(variable.Key.ToString()))
+                {
+                    stringBuilder.Replace(variable.Key.ToString(), variable.Value.ToString());
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
